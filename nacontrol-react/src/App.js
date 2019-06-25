b74299aa7b5e689a4870b2a113b94b3298a0e042
Juliddanno Tghjeghjijueira Cortez Ga22rjuliacia <juliasnnotcg@hotghjmail.com>
2019-06-25 20:19:50 -0300

    asd

--- nacontrol-react/src/App.js
+++ nacontrol-react/src/App.js
@@ -1,37 +1,20 @@
 import React, { Component } from 'react';
 
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+
 import Routes from './routes';
-
-
+import GoogleMaps from './components/Maps/GoogleMaps';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Routes />
-        <Map google={this.props.google} zoom={14}>
-
-          <Marker onClick={this.onMarkerClick}
-            name={'Current location'} />
-
-          <InfoWindow onClose={this.onInfoWindowClose}>
-
-          </InfoWindow>
-        </Map>
+        <GoogleMaps/>
       </div>
 
 
     );
-
-    
   }
-
-
-
-  
 }
 
-export default GoogleApiWrapper({
-  apiKey: ("")
-})(App)
+export default App;
