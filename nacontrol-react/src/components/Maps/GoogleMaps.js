72adf33c93c8d88298748c87ee010a5d5d04aa57
Juliddanno Tghjeghjijueira Cortez Ga22rjuliacia <juliasnnotcg@hotghjmail.com>
2019-06-25 20:35:13 -0300

    Subindo

--- nacontrol-react/src/components/Maps/GoogleMaps.js
+++ nacontrol-react/src/components/Maps/GoogleMaps.js
@@ -3,22 +3,58 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 
 export class GoogleMaps extends Component {
+  state = {
+    activeMarker: {},
+    selectedPlace: {},
+    showingInfoWindow: false
+  };
+
+  onMarkerClick = (props, marker) =>
+  this.setState({
+    activeMarker: marker,
+    selectedPlace: props,
+    showingInfoWindow: true
+  });
+
+  onMapClicked = () => {
+    if (this.state.showingInfoWindow)
+      this.setState({
+        activeMarker: null,
+        showingInfoWindow: false
+      });
+  };
+
     render(){
         return(
             <Map google={this.props.google} zoom={14}>
 
-            <Marker onClick={this.onMarkerClick}
-              name={'Current location'} />
-  
-            <InfoWindow onClose={this.onInfoWindowClose}>
-  
-            </InfoWindow>
+            <Marker 
+              onClick={this.onMarkerClick}
+              name={'Endereço do grupo'}
+              position={{ lat: 37.779519, lng: -122.40564 }}
+              title="The marker`s title will appear as a tooltip." />
+
+            <Marker
+              onClick={this.onMarkerClick}
+              name="SOMA"
+              position={{ lat: 37.778519, lng: -122.40564 }}
+              title="The marker`s title will appear as a tooltip." />
+
+              <InfoWindow
+                marker={this.state.activeMarker}
+                onClose={this.onInfoWindowClose}
+                visible={this.state.showingInfoWindow}>
+                <div>
+                  <h1>{this.state.selectedPlace.name}</h1>
+                </div>
+              </InfoWindow>
+
           </Map>
         );
     }
 }
 
-export default GoogleMaps({
-    apiKey: ("")
-  })
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyDryBws_KN0rstpvoaihKGxLBGgxEnQtTY")
+  })(GoogleMaps)
   
