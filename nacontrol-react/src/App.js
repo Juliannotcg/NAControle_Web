0db84567face01d241971984534ef67f413190a2
Julianno Teixeira Garcia <T088166146@cassi.com.br>
2019-06-25 08:53:13 -0300

    IMplementações.

--- nacontrol-react/src/App.js
+++ nacontrol-react/src/App.js
@@ -6,6 +6,35 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        infraestruturas: [],
+        mainLoading: false,
+        mensagemErro: null,
+        filtro: {},
+        instalacoes: []
+    }
+
+    this.buscar = this.buscar.bind(this);
+    this.mostrarMensagemErro = this.mostrarMensagemErro.bind(this);
+    this.buscarInstalacoes = this.buscarInstalacoes.bind(this);
+}
+  
+
+  buscar() {
+    this.setState({ mainLoading: true });
+
+    API.Infraestrutura.post("/infraestrutura/busca", this.state.filtro)
+    .then(infraestruturas => {
+        this.setState({ infraestruturas: infraestruturas, mainLoading: false })
+    }, (evt) => this.mostrarMensagemErro({ mainLoading: false }, evt));
+}
+  
+  
+  
+  
   render() {
     return (
       <div className="App">
