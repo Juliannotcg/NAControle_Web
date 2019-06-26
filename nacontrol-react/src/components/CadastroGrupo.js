3c22b4440569a6a359470b6937c8d8ca18dfca57
Julianno Teixeira Garcia <T088166146@cassi.com.br>
2019-06-26 14:45:35 -0300

    IMplementando o cadastro do grupo.

--- nacontrol-react/src/components/CadastroGrupo.js
+++ nacontrol-react/src/components/CadastroGrupo.js
@@ -10,85 +10,79 @@ export default function CadastroGrupo (){
     return (
         <React.Fragment>
           <Typography variant="h6" gutterBottom>
-            Shipping address
+            Cadastro de grupo
           </Typography>
           <Grid container spacing={3}>
             <Grid item xs={12} sm={6}>
               <TextField
                 required
                 id="firstName"
-                name="firstName"
-                label="First name"
+                name="Nome do grupo"
+                label="Nome do grupo"
                 fullWidth
-                autoComplete="fname"
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
                 required
-                id="lastName"
-                name="lastName"
-                label="Last name"
+                id="dataFundacao"
+                name="dataFundacao"
+                label="Data de fundação"
                 fullWidth
-                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                required
+                id="cep"
+                name="cep"
+                label="CEP"
+                fullWidth
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
                 required
-                id="address1"
-                name="address1"
-                label="Address line 1"
+                id="endereco"
+                name="endereco"
+                label="Endereço completo"
                 fullWidth
-                autoComplete="billing address-line1"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                id="address2"
-                name="address2"
-                label="Address line 2"
-                fullWidth
-                autoComplete="billing address-line2"
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
                 required
-                id="city"
-                name="city"
-                label="City"
+                id="cidade"
+                name="cidade"
+                label="Cidade"
                 fullWidth
-                autoComplete="billing address-level2"
               />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField id="state" name="state" label="State/Province/Region" fullWidth />
+              <TextField id="state" name="estado" label="Estado" fullWidth />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
                 required
-                id="zip"
-                name="zip"
-                label="Zip / Postal code"
+                id="latitude"
+                name="latitude"
+                label="Latitude"
                 fullWidth
-                autoComplete="billing postal-code"
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
                 required
-                id="country"
-                name="country"
-                label="Country"
+                id="longitude"
+                name="longitude"
+                label="Longitude"
                 fullWidth
-                autoComplete="billing country"
               />
             </Grid>
             <Grid item xs={12}>
               <FormControlLabel
                 control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-                label="Use this address for payment details"
+                label="O grupo está ativo."
               />
             </Grid>
           </Grid>
