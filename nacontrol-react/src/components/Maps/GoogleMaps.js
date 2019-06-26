08ab4f92038dd7f00f6bcb7744bebe625a5c0260
Juliddanno Tghjeghjijueira Cortez Ga22rjuliacia <juliasnnotcg@hotghjmail.com>
2019-06-25 23:52:21 -0300

    Subindo

--- nacontrol-react/src/components/Maps/GoogleMaps.js
+++ nacontrol-react/src/components/Maps/GoogleMaps.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
+const grupo = {
+  nome: null,
+  endereco: {
+    logradouro: null,
+    lote: null,
+    rua: null,
+    quadra: null,
+    cep: null,
+    cidade: null,
+    uf: null,
+    latitude: null,
+    longitude: null,
+  }
+}
+
 
 export class GoogleMaps extends Component {
   state = {
     activeMarker: {},
     selectedPlace: {},
     showingInfoWindow: false,
-    dadosGrupo: {
-      nome: null,
-      endereco: {
-        logradouro: null,
-        lote: null,
-        rua: null,
-        quadra: null,
-        cep: null,
-        cidade: null,
-        uf: null,
-        latitude: null,
-        longitude: null,
-      }
-    }
+    grupos: [grupo]
   };
 
   onMarkerClick = (props, marker) =>
@@ -42,17 +44,16 @@ export class GoogleMaps extends Component {
         return(
             <Map google={this.props.google} zoom={14}>
 
-            <Marker 
-              onClick={this.onMarkerClick}
-              name={'Endereço do grupo'}
-              position={{ lat: 37.779519, lng: -122.40564 }}
-              title="The marker`s title will appear as a tooltip." />
 
-            <Marker
-              onClick={this.onMarkerClick}
-              name="SOMA"
-              position={{ lat: 37.778519, lng: -122.40564 }}
-              title="The marker`s title will appear as a tooltip." />
+             {this.state.grupos.map((value) => {
+
+                <Marker 
+                    onClick={this.onMarkerClick}
+                    name={value.nome}
+                    position={{ lat: value.endereco.latitude, lng: value.endereco.latitude }}
+                    title="The marker`s title will appear as a tooltip." />
+
+                  })}
 
               <InfoWindow
                 marker={this.state.activeMarker}
