afa39e048378a171e911e614a22664cfb5b388b7
Julianno Teixeira Garcia <T088166146@cassi.com.br>
2019-07-09 17:23:33 -0300

    asd

--- nacontrol-react/src/components/CadastroGrupo.js
+++ nacontrol-react/src/components/CadastroGrupo.js
@@ -1,93 +1,104 @@
+import React, { Component } from 'react';
 
-import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
-export default function CadastroGrupo (){
-    return (
-        <React.Fragment>
-          <Typography variant="h6" gutterBottom>
-            Cadastro de grupo
-          </Typography>
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="firstName"
-                name="Nome do grupo"
-                label="Nome do grupo"
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="dataFundacao"
-                name="dataFundacao"
-                type="date"
-                label="Data de abertura"
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} sm={3}>
-              <TextField
-                required
-                id="cep"
-                name="cep"
-                label="CEP"
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                id="endereco"
-                name="endereco"
-                label="Endereço completo"
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="cidade"
-                name="cidade"
-                label="Cidade"
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField id="state" name="estado" label="Estado" fullWidth />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="latitude"
-                name="latitude"
-                label="Latitude"
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="longitude"
-                name="longitude"
-                label="Longitude"
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-                label="O grupo está ativo."
-              />
-            </Grid>
-          </Grid>
+export default class CadastroGrupo extends Component() {
+  constructor(props) {
+    super(props);
 
-        </React.Fragment>
-      );
+    this.state = {
+      
     }
+}
+  render() {
+
+    return (
+      <React.Fragment>
+        <Typography variant="h6" gutterBottom>
+          Cadastro de grupo
+          </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="firstName"
+              name="Nome do grupo"
+              label="Nome do grupo"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="dataFundacao"
+              name="dataFundacao"
+              type="date"
+              label="Data de abertura"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <TextField
+              required
+              id="cep"
+              name="cep"
+              label="CEP"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="endereco"
+              name="endereco"
+              label="Endereço completo"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="cidade"
+              name="cidade"
+              label="Cidade"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField id="state" name="estado" label="Estado" fullWidth />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="latitude"
+              name="latitude"
+              label="Latitude"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="longitude"
+              name="longitude"
+              label="Longitude"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
+              label="O grupo está ativo."
+            />
+          </Grid>
+        </Grid>
+        <Button color="primary" className={classes.button}>
+          Salvar
+      </Button>
+      </React.Fragment>)
+  }
+}
